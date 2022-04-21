9057ebe7940e8b732cc2c8594c8fcf437310ba29
Bashar-Owainat <owainatbashar13@gmail.com>
2022-04-21 08:27:48 +0300

    lab 13

---
+++ AsyncInnApp/AsyncInnApp/Models/Interfaces/IHotel.cs
@@ -0,0 +1,17 @@
+﻿using AsyncInnApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInnApp.Interfaces
+{
+    public interface IHotel
+    {
+        Task<Hotel> Create(Hotel hotel);
+        Task<List<Hotel>> GetStudents();
+        Task<Hotel> GetStudent(int id);
+        Task<Hotel> UpdateStudent(int id, Hotel hotel);
+        Task Delete(int id);
+    }
+}
---
+++ AsyncInnApp/AsyncInnApp/Models/Services/HotelServices.cs
@@ -0,0 +1,31 @@
+﻿using AsyncInnApp.Data;
+using AsyncInnApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AsyncInnApp.Models.Services
+{
+    public class HotelServices 
+    {
+        private readonly AsyncInnDbContext _context;
+
+        public HotelServices(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Hotel>>> GetHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+       
+
+    }
+}
